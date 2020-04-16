1494bcdf8d6a0accda812abaed9657c12d4bdf81
CPallares96 <urielesc@uninorte.edu.co>
2020-04-15 22:27:36 -0500

    .

--- scrip.js
+++ scrip.js
@@ -60,7 +60,7 @@ function insertNewRecord(data) {
     cell13 = newRow.insertCell(12);
     cell13.innerHTML =  `<a onClick="onEdit(this)">Edit</a>
                          <a onClick="onDelete(this)">Delete</a>`;
-    console.log(data);
+    console.log(data.tcarro);
 
 }
 
